95f59d1a9b3500fcc1fced9c3c86b05b70bf1257
Mohammed Al Ashaal <m7medalash3al@gmail.com>
2017-05-05 18:39:32 +0200

    Create main.php

---
+++ main.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: Wordpress Allowed Hosts
+ * Plugin URI: https://github.com/alash3al/wp-allowed-hosts
+ * Description: A plugin that protectet your wordpress installation from spoofed HTTP requests .
+ * Version: 1.0
+ * Author: Mohammed Al Ashaal
+ * Author URI: http://alash3al.xyz/
+ */
+if ( function_exists("add_action") ) {
+	function alash3al_allowed_hosts(){
+		if ( defined("WP_ALLOWED_HOSTS") && ! empty(WP_ALLOWED_HOSTS) ) {
+			$allowed = str_replace(",", "|", preg_replace('~\s~', "", WP_ALLOWED_HOSTS));
+			$die = false;
+			if ( $_SERVER["SERVER_NAME"] !== $_SERVER["HTTP_HOST"] ) {
+				$die = true;
+			} else if ( ! preg_match("~^". ($allowed) ."$~", $_SERVER["SERVER_NAME"]) ) {
+				$die = true;
+			}
+			if ( $die ) {
+				header("Location: " . get_home_url());
+				die();
+			}
+		}
+	}
+	add_action("init", "alash3al_allowed_hosts");
+}
