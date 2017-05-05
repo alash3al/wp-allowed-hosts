90321fc1b62b55cadb62e00d1874a9e4737b66c6
Mohammed Al Ashaal <m7medalash3al@gmail.com>
2017-05-05 18:48:06 +0200

    Update main.php

--- main.php
+++ main.php
@@ -7,22 +7,18 @@
  * Author: Mohammed Al Ashaal
  * Author URI: http://alash3al.xyz/
  */
-if ( function_exists("add_action") ) {
-	function alash3al_allowed_hosts(){
-		if ( defined("WP_ALLOWED_HOSTS") && ! empty(WP_ALLOWED_HOSTS) ) {
-			$allowed = str_replace(",", "|", preg_replace('~\s~', "", WP_ALLOWED_HOSTS));
-			$die = false;
-			if ( $_SERVER["SERVER_NAME"] !== $_SERVER["HTTP_HOST"] ) {
-				$die = true;
-			} else if ( ! preg_match("~^". ($allowed) ."$~", $_SERVER["SERVER_NAME"]) ) {
-				$die = true;
-			}
-			if ( $die ) {
-				header("Location: " . get_home_url());
-				die();
-			}
+if ( function_exists("get_home_url") ) {
+	if ( defined("WP_ALLOWED_HOSTS") && ! empty(WP_ALLOWED_HOSTS) ) {
+		$allowed = str_replace(",", "|", preg_replace('~\s~', "", WP_ALLOWED_HOSTS));
+		$die = false;
+		if ( $_SERVER["SERVER_NAME"] !== $_SERVER["HTTP_HOST"] ) {
+			$die = true;
+		} else if ( ! preg_match("~^". ($allowed) ."$~", $_SERVER["SERVER_NAME"]) ) {
+			$die = true;
+		}
+		if ( $die ) {
+			header("Location: " . get_home_url());
+			die();
 		}
 	}
-	add_action("init", "alash3al_allowed_hosts");
 }
-
